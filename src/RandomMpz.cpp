5b17769ded6700bccec4434a6347b3ed6d045016
dingxin <wdx7266@vip.qq.com>
2018-08-07 11:41:00 +0800

    update

--- src/RandomMpz.cpp
+++ src/RandomMpz.cpp
@@ -12,7 +12,11 @@ int main(int argc, char **argv)
     mcl::gmp::getRand(mpz, 1024);
     std::cout << mpz.get_str() << std::endl;
     std::cout << "----------------------------------" << std::endl;
+    std::cout << mpz.get_str(16) << std::endl;
+    std::cout << "----------------------------------" << std::endl;
     mcl::gmp::getRand(mpz, 1024, rg);
     std::cout << mpz.get_str() << std::endl;
+    std::cout << "----------------------------------" << std::endl;
+    std::cout << mpz.get_str(16) << std::endl;
     return 0;
 }
